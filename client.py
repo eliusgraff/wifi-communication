786286273d16fcf17463b8b60c61ae45999e2f8d
Elius <graffe1@spu.edu>
2020-11-13 14:58:31 -0800

    Revert "working on getting stream working"
    This reverts commit 8c573937341429ff79a869cadb4caa9846613158.

--- client.py
+++ client.py
@@ -1,51 +1,11 @@
 #client script
 import socket
 
-HOST = '10.0.0.207' #this will need to be adjusted for server's ip address
-HOST = socket.gethostname()
+HOST = '10.0.0.129'
 PORT = 1234
-HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
-while True:
-    full_msg = ''
-    new_msg = True
-    while True:
-        msg = s.recv(16)
-        if new_msg:
-            #print(f"new message length: {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
-
-        full_msg += msg.decode("utf-8")
-
-        if len(full_msg)-HEADERSIZE == msglen:
-            #print("full message recieved:")
-            yval = full_msg[HEADERSIZE:]
-            print(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = ''
-
-            msg = s.recv(16)
-        if new_msg:
-            #print(f"new message length: {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
-
-        full_msg += msg.decode("utf-8")
-
-        if len(full_msg)-HEADERSIZE == msglen:
-            #print("full message recieved:")
-            xval = full_msg[HEADERSIZE:]
-            print(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = ''
-
-        print("x value:", xval, "  y value:", yval)
-
-    print(full_msg)
-
-
-    print(msg)
+msg = s.recv(8)
+print(msg.decode("utf-8")
--- server.py
+++ server.py
@@ -3,39 +3,15 @@ import socket
 #from gpiozero import PWMLED, MCP3008
 from time import sleep
 
-#yinput = MCP3008(0)
-#xinput = MCP3008(1)
-HOST = socket.gethostname()
+HOST = '10.0.0.129'
 PORT = 1234
-HEADERSIZE = 10
-print("hosting on ", HOST)
+print(HOST)
 
-myServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-myServer.bind((HOST,PORT))
-myServer.listen(1)
-
-#saddress = False
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(4)
 
 while True:
-	client, address = myServer.accept()
+	clientsocket, address = s.accept()
 	print("connection from", address, " has been established!")
-
-	msg = "Welcome to Dynamic Securities server!"
-	msg = f'{len(msg):<{HEADERSIZE}}'+msg
-	client.send(bytes(msg,"utf-8"))
-
-	while True:
-
-		msg = random.random()
-		msg = f'{len(msg):<{HEADERSIZE}}' + msg
-		client.send(msg)
-
-		sleep(0.1)
-
-'''
-	print("connection with ", address, " closed")
-	client.close()
-	msg = input("enter y to listen again or n to close server: ")
-	if msg == 'n':
-		break
-'''
+	clientsocket.send("welcome to the server")
