a58d5d5c2cc46a94e463eef0f79b9ad8f33dc95c
Elius <graffe1@spu.edu>
2020-11-14 21:16:52 -0800

    i should delete this branch now

--- server.py
+++ server.py
@@ -1,17 +1,39 @@
 #SERVER SCRIPT
 import socket
-#from gpiozero import PWMLED, MCP3008
+import random
 from time import sleep
 
-HOST = '10.0.0.129'
+HOST = socket.gethostname()
 PORT = 1234
-print(HOST)
+HEADERSIZE = 10
+print("hosting on ", HOST)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST,PORT))
-s.listen(4)
+myServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+myServer.bind((HOST,PORT))
+myServer.listen(1)
+address = False
 
 while True:
-	clientsocket, address = s.accept()
+	client, address = myServer.accept()
 	print("connection from", address, " has been established!")
-	clientsocket.send("welcome to the server")
+	msg = "Welcome to Dynamic Securities server!"
+	msg = f'{len(msg):<{HEADERSIZE}}'+msg
+	client.send(bytes(msg,"utf-8"))
+
+	while True:
+		#msg = input("data to send: ") 
+		msg = str(random.random())
+		print(msg)
+		if msg == "break":
+			break
+
+		msg = f'{len(msg):<{HEADERSIZE}}' + msg
+		client.send(bytes(msg, "utf-8"))
+
+		sleep(0.1)
+
+	print("connection with ", address, " closed")
+	client.close()
+	msg = input("enter y to listen again or n to close server: ")
+	if msg == 'n':
+		break 
